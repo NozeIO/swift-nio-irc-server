6babb18a9572ad50e9c7f7223a893ee9516a5181
Helge Heß <devteam@zeezide.de>
2018-04-26 15:10:58 +0200

    Fix compilation on Linux
    ... signal needs import for Glibc

--- Sources/miniircd/main.swift
+++ Sources/miniircd/main.swift
@@ -12,6 +12,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#if os(Linux)
+  import Glibc
+#endif
 import func Dispatch.dispatchMain
 import NIO
 import NIOIRC
