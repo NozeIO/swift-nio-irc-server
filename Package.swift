389ec8028b91cdceb2c7b340d2b4b533ee1031f5
Helge Heß <devteam@zeezide.de>
2018-04-26 15:31:06 +0200

    Help SPM resolve repositories
    Gets confused when the suffixes are missing.

--- Package.swift
+++ Package.swift
@@ -11,11 +11,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", 
                  from: "1.5.1"),
-        .package(url: "https://github.com/NozeIO/swift-nio-irc",
+        .package(url: "https://github.com/NozeIO/swift-nio-irc.git",
                  from: "0.5.0"),
-        .package(url: "https://github.com/NozeIO/swift-nio-irc-eliza",
-                 from: "0.5.0"),
-        .package(url: "https://github.com/NozeIO/swift-nio-irc-webclient",
+        .package(url: "https://github.com/NozeIO/swift-nio-irc-eliza.git",
+                 from: "0.5.1"),
+        .package(url: "https://github.com/NozeIO/swift-nio-irc-webclient.git",
                  from: "0.5.1")
     ],
     targets: [
