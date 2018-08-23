d6b4ac993872d9f36fa150cc551cab4025c6053f
Helge Heß <devteam@zeezide.de>
2018-08-23 18:54:23 +0200

    Use SwiftNIO 1.9.2
    ... no other changes.

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.8.0"),
+                 from: "1.9.2"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-irc.git",
-                 from: "0.6.0"),
+                 from: "0.6.1"),
         .package(url: "https://github.com/NozeIO/swift-nio-irc-eliza.git",
                  from: "0.5.2"),
         .package(url: "https://github.com/NozeIO/swift-nio-irc-webclient.git",
-                 from: "0.5.4")
+                 from: "0.6.1")
     ],
     targets: [
         .target(name: "IRCServer", dependencies: [ "NIO", "NIOIRC" ]),
